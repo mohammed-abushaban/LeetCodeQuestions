3057984bcd71eec2bd700b04e2268af0aa4de354
Mohammed AbuShaban <m.aboushaaban@gmail.com>
2022-07-09 19:06:39 +0300

    �

---
+++ FirstUniqChar.java
@@ -0,0 +1,11 @@
+public class FirstUniqChar {
+    public static int firstUniqChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (s.indexOf(c) == s.lastIndexOf(c)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
