caee915c4f1b6aa20bb03925ddd34fda5e89013f
Mohammed AbuShaban <m.aboushaaban@gmail.com>
2022-07-10 23:06:31 +0300

    �

--- SearchMatrix.java
+++ SearchMatrix.java
@@ -9,7 +9,7 @@ public class SearchMatrix {
      * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
      * Output: true
      */
-    public static boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix(int[][] matrix, int target) {
         for (int i = 0; i < matrix.length; i++) {
             if (matrix[i][matrix[i].length - 1] >= target) {
                 for (int j = 0; j < matrix[i].length; j++) {
