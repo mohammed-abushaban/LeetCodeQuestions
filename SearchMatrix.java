4fd97fc930ee7d265c58b0c7993f715d0cc99882
Mohammed AbuShaban <m.aboushaaban@gmail.com>
2022-07-10 23:04:35 +0300

    �

--- SearchMatrix.java
+++ SearchMatrix.java
@@ -1,16 +1,24 @@
 public class SearchMatrix {
-    public static void main(String[] args) {
-        int[][] arr = new int[][] { { 1 } };
-        System.out.println(searchMatrix(arr, 13));
-    }
-
+    /*
+     * Write an efficient algorithm that searches for a value target in an m x n
+     * integer matrix matrix. This matrix has the following properties:
+     * 
+     * Integers in each row are sorted from left to right.
+     * The first integer of each row is greater than the last integer of the
+     * previous row.
+     * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+     * Output: true
+     */
     public static boolean searchMatrix(int[][] matrix, int target) {
         for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i][0] > target) {
-                for (int j = 0; j < matrix[i - 1].length; j++) {
-                    if (matrix[i - 1][j] == target)
+            if (matrix[i][matrix[i].length - 1] >= target) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (matrix[i][j] == target) {
+                        System.out.println(matrix[i][j]);
                         return true;
+                    }
                 }
+                return false;
             }
         }
         return false;
